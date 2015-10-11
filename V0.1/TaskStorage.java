d22662461bd6b27f9290adec121f6a108c594db0
timothywee <wee.laiheng.timothy@gmail.com>
2015-10-11 15:15:06 +0800

    Updated TaskStorage, add default.txt

--- V0.1/TaskStorage.java
+++ V0.1/TaskStorage.java
@@ -4,27 +4,79 @@ import java.util.*;
 
 public class TaskStorage {
 
-	private String filename;
+	private static String fileName = "default.txt";
+	private static BufferedReader br = null;
+	private static BufferedWriter bw = null;
+	
+	// Temporary internal arrayList for storage
+	private static ArrayList<String> taskList = new ArrayList<String>();
 	
 	public TaskStorage() {
-		//Initialise the variables;
-		
+		//Initialise the variables;		
 	}
 
-	public void setFileURL(String fileURL){
-
+	public static void setFileURL(String fileURL){
+		fileName = fileURL + ".txt";
 	}
 
-	public void rewriteContent(ArrayList<String> allTasks){
-
+	public static void rewriteContent(ArrayList<String> allTasks) {
+		taskList = allTasks;
+		try {  
+			writeFileByLine();
+		} catch (IOException e) {
+			handleException(e);
+		} finally {
+			try {
+				if (bw != null) {
+					bw.close();
+				}   					
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	private static void handleException(IOException e) {
+		e.printStackTrace();
+		System.exit(0);
 	}
 
-	public ArrayList<String> readContent(){
-		ArrayList<String> fileContent = new ArrayList<String>();
-		fileContent.add("task1");
-		fileContent.add("task2");
-		fileContent.add("task3");
-		return fileContent;
+	public static ArrayList<String> readContent() throws IOException {
+		try {
+			readFileByLine();
+        } catch (IOException e) {
+        	handleException(e);
+        } finally {
+        	closeReader();
+        }
+		return taskList;
+	}
+	
+	private static void readFileByLine() throws FileNotFoundException,
+	IOException {
+		BufferedReader br = new BufferedReader(new FileReader(fileName));
+		ArrayList<String> updatedTaskList = new ArrayList<String>();
+		String line = null;
+		while ((line = br.readLine()) != null) {
+			updatedTaskList.add(line);
+		}
+		taskList = updatedTaskList;
+		br.close();
+	}
+	
+	private static void writeFileByLine() throws IOException {
+		bw = new BufferedWriter(new FileWriter(fileName));
+		for (int i = 0; i < taskList.size(); i++) { 
+		  bw.write(taskList.get(i));
+		  bw.newLine();
+		}
+	}
+
+	private static void closeReader() throws IOException {
+		if (br != null) {
+			br.close();
+		}
 	}
 
 }
+
