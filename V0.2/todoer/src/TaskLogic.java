5d3bd541b32ceea7da4ba7996db375c1d6a045e5
mrenoon <root@vicom-ubuntu>
2015-10-14 09:56:29 +0800

    V0.2

---
+++ V0.2/todoer/src/TaskLogic.java
@@ -0,0 +1,58 @@
+
+import java.io.*;
+import java.util.*;
+
+
+public class TaskLogic {
+
+	private static String ENTER = "Enter";
+    private String dataFile = "data.txt";
+
+	TaskCommandParse mTaskCommandParse = new TaskCommandParse();
+	private final String messageSuccessful = "Successful";
+	private ArrayList<String> allTasks;
+	private TaskStorage mTaskStorage;
+
+	
+	public TaskLogic() {
+		//Initialise the variables;
+		mTaskStorage = new TaskStorage();
+		mTaskStorage.setFileURL(dataFile);
+
+		allTasks = mTaskStorage.readContent();
+	}
+
+	protected String process(String userCommand) {
+		String command = "", taskInfo = "";
+		String[] commandInfo = mTaskCommandParse.getCommandInfo(userCommand);
+		command = commandInfo[0];  taskInfo = commandInfo[1];
+		
+		switch (command){
+			case "add":
+				allTasks.add(taskInfo);
+				mTaskStorage.rewriteContent(allTasks);
+				return "Successfully added '" + taskInfo + "'\n";
+			case "showall":
+				return showAll();
+			case "delete":
+				String deleted = allTasks.get(Integer.parseInt(taskInfo));
+				allTasks.remove(Integer.parseInt(taskInfo));
+				return "deleted '" + deleted + "'.\n";
+			case "exit":
+				return null;
+			default:
+				return "Syntax error: command(" + command + ") not found.\n";
+		}
+		
+	}
+
+	protected String showAll() {
+		String result = "";
+		for (int i=0;i<allTasks.size();i++){
+			result += allTasks.get(i)+"\n";
+		}
+		return result;
+	}
+
+
+}
